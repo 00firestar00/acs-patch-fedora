00a351256d50326658c2b275d255593b9fbe8512
Evan Jarrett <ejfirestar00@gmail.com>
2019-10-31 20:45:28 -0700

    script for generating patch file for fedora

---
+++ fedpkg.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+wget -O acso.patch https://gitlab.com/Queuecumber/linux-acs-override/raw/master/workspaces/5.1/acso.patch
+echo "From: Evan Jarrett <evan@evanjarrett.com>
+Subject: [PATCH] acso patch" | cat - acso.patch > tmp && mv tmp acso.patch
+./scripts/newpatch.sh acso.patch
+echo "---" >> acso.patch
+make release
+git config --global commit.gpgsign false
+fedpkg local
+git config --global commit.gpgsign true
